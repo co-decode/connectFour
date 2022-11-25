1930a068fae897cda06065d482f20c6302bfd404
code <codecode60@protonmail.com>
2022-11-25 15:57:02 +0930

    working on gameboard

--- pages/gameboard.tsx
+++ pages/gameboard.tsx
@@ -1,13 +1,43 @@
+import { MouseEvent, useEffect, useRef, useState } from 'react'
 import styles from '../styles/gameboard.module.css'
+interface Position {
+    row: number,
+    col: number
+}
+
+const board =  Array.from({length: 42}, _ => 0)
 
 export default function GameBoard() {
+    const [piecePos, setPiecePos] = useState<number>(3)
+    const pieceRef = useRef<null | HTMLDivElement>(null)
+    const holeRef = useRef<HTMLDivElement[]>([])
+
+    useEffect(() => {
+        if (pieceRef.current == null ) return
+        let offset = piecePos - 3
+        pieceRef.current.style.left = `calc(50% + ${offset * 100}px)`
+    },[piecePos])
+
+    const handleHover = (col: number) => {
+        setPiecePos(col)
+        for (let i = 0; i < 41; i++) {
+            if (i % 7 === col) holeRef.current[i].style.backgroundColor = "yellow"
+        }
+    }
     return (
-        <>
-        {<div className={styles.board}>
-        
-           
-        </div>}
-        </>
+        <div style={{width:'100%', height:'100vh', position:'relative'}}>
+        <div className={styles.piece} ref={pieceRef} style={{position:'absolute'}}/>
+        <div className={styles.board}>
+        {
+       board.map((v,i) => 
+            <div 
+                ref={el => {if (el == null) return; holeRef.current.push(el)}}
+                key={`r${Math.floor(i / 7)}c${i % 7}}`} 
+                className={styles.hole} 
+                onMouseOver={(e: MouseEvent)=>handleHover(i%7)}/>)
+        }
+        </div>
+        </div>
     )
 }
 
