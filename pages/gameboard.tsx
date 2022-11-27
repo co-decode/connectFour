540dff755019d5c00759213f6d92142f071b45b6
code <codecode60@protonmail.com>
2022-11-27 15:41:46 +0930

    finished game win logic

--- pages/gameboard.tsx
+++ pages/gameboard.tsx
@@ -4,7 +4,21 @@ interface Position {
     row: number,
     col: number
 }
+/* 
+interface Helper {
+    horizontal: number[][],
+    vertical: number[][],
+    inclineDiag: number[][],
+    declineDiag: number[][]
+}
 
+const helper: Helper = {
+    horizontal: [[0,1],[0,-1]],
+    vertical: [[1,0],[-1,0]],
+    inclineDiag: [[1,1],[-1,-1]],
+    declineDiag: [[1,-1],[-1,1]]
+}
+ */
 const [RED, BLUE] = ['RED', 'BLUE']
 
 const board =  Array.from({length: 42}, _ => 0)
@@ -12,14 +26,19 @@ const board =  Array.from({length: 42}, _ => 0)
 /* What needs to happen next? */
 /* 
 - DONE Fix piece spawn in bug
-- NEXT ->> Determine when the game is won.
+- DONE Determine when the game is won.
+- Next ->> Fix Spawn in issues with multiple bad highlights
 - Then work on socket integration.
 */
 export default function GameBoard() {
     const [pieces, setPieces] = useState<number[]>([0])
     const [piecePos, setPiecePos] = useState<number>(3)
-    const [moves, setMoves] = useState<number[]>([...board])
+    const [moves, setMoves] = useState<number[][]>(
+        Array.from({length:6}, (_,i) => 
+            Array.from({length: 7}, () => 0))
+        )
     const [turn, setTurn] = useState<string>(RED)
+    const [gameOver, setGameOver] = useState<boolean>(false)
     const pieceRef = useRef<null | HTMLDivElement>(null)
     const holeRef = useRef<HTMLDivElement[]>([])
 
@@ -34,12 +53,20 @@ export default function GameBoard() {
 
     },[piecePos,pieces])
 
+    useEffect(() => {
+        if (gameOver) {
+            holeRef.current.forEach(hole => hole.style.cursor = "default")
+        
+        }
+    }, [gameOver])
+
     function paintLowest(col:number) {
         let lowestOfCol: number | null = null;
         // No hole should be yellow if no space if available
         for (let i = 0; i < 42; i++) {
             // Ensure that the lowest unoccupied space in column is selected
-            if (i % 7 === col && moves[i] === 0) lowestOfCol = i 
+            
+            if (i % 7 === col && moves[Math.floor(i / 7)][col] === 0) lowestOfCol = i 
         }
         // Guard against null case
         if (lowestOfCol == null) return
@@ -49,11 +76,14 @@ export default function GameBoard() {
 
     useEffect(() => {
         // Only when a piece is added does a new paint occur, such that even without a mouseevent causing a repaint, a yellow circle will appear in the column where the cursor is hovering.
+        // Ensure paint does not occur after the winning move.
         let col = piecePos
-        paintLowest(col)
+        if (!gameOver) paintLowest(col)
     },[pieces])
 
     const handleHover = (col: number) => {
+        // Do not highlight if the game has been won:
+        if (gameOver) return
         // Change piecePos and paint lowest on mouseOver event
         setPiecePos(col)
         paintLowest(col)
@@ -64,19 +94,58 @@ export default function GameBoard() {
         }
         // All backgroundColors are cleared, but the hover event fire AFTER this event, ensuring there should always be an open space in yellow so long as the board is being hovered.
     }
+    function checkMove(r:number, c:number, t:number): boolean {
+        // Check count for vertical, horizontal, incline diagonal and decline diagonal
+        // let center = moves[r][c]
+        let count = 1
+        let i = 1
+        // HORIZONTAL:
+        while (moves[r][c + i++] === t) count++
+        i = 1
+        while (moves[r][c - i++] === t) count++
+        if (count >= 4) return true
+        console.log(count)
+        i = 1
+        count = 1
+        // VERTICAL:
+        while (r + i < 6 && moves[r + i++][c] === t) count++
+        if (count === 4) return true
+        console.log(count)
+        i = 1
+        count = 1
+        // INCLINE DIAG:
+        while (r + i < 6 && moves[r + i][c + i++] === t) count++
+        i = 1
+        while (r - i >= 0 && moves[r - i][c - i++] === t) count++
+        console.log(count)
+        if (count >= 4) return true
+        i = 1
+        count = 1
+        // DECLINE DIAG:
+        while (r - i >= 0 && moves[r - i][c + i++] === t) count++
+        i = 1
+        while (r + i < 6 && moves[r + i][c - i++] === t) count++
+        console.log(count)
+        if (count >= 4) return true
+        i = 1
+        count = 1
+
+        return false
+    }
     const handlePlacement = (col:number) => {
         if (pieceRef.current === null) return
         let lowestOfCol:number | null = null;
         // Loop through the game board and discover the lowest of Col
         for (let i = 0; i < 42; i++) {
-            if (i % 7 === col && moves[i] === 0) lowestOfCol = i
+            if (i % 7 === col && moves[Math.floor(i / 7)][col] === 0) lowestOfCol = i
         }
         // Guard against placing on a column that has no space
         if (lowestOfCol == null) return
         // Add the move to the list of occupied circles
-        setMoves(moves.map((v,i) => i === lowestOfCol ? (turn === RED ? 1 : 2) : v))
-        // Check which row the move will occupy 
+        // setMoves(moves.map((v,i) => i === lowestOfCol ? (turn === RED ? 1 : 2) : v))
         let row = Math.floor(lowestOfCol / 7)
+        setMoves(moves.map((v,r) => r === row ? v.map((prev,c) => c === col ? (turn === RED ? 1 : 2) : prev) : v))
+        // Check which row the move will occupy 
         // Piece translates, there should be a transition property set for this.
         pieceRef.current.style.transform = `translate(-50%, ${600 - (5 - row) * 100}px)`
         // Release highlighting
@@ -88,10 +157,13 @@ export default function GameBoard() {
             setPieces([...pieces, 0])
             setTurn(turn === RED ? BLUE : RED)
         }
-        setTimeout(afterDelay, 300)
+        // Check if the game has been won
+        if (!checkMove(row, col, turn === RED ? 1 : 2)) setTimeout(afterDelay, 300)
+        else setGameOver(true)
     }
     return (
         <div style={{width:'100%', height:'100vh', position:'relative'}}>
+            {gameOver ? <h1>THE GAME IS WON! A WINNER IS {turn}!</h1> : null}
         {pieces.map((v,i) =>
             <div 
                 key={i}
