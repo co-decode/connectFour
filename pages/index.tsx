d8758c24fa5074338c9a5fe50014dceed23a1156
code <codecode60@protonmail.com>
2022-12-02 12:27:55 +0930

    setting up testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
+// import Head from 'next/head'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Client from '../components/client'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div role="heading" aria-label="homeContainer" className={styles.container}>
       <Client />
     </div>
   )
